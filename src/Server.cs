0d0a004861e58890457513f81c14a8ebf72b6866
GlaticuS <glaticus@mail.ru>
2024-11-03 22:44:51 +0300

    completed

--- src/Server.cs
+++ src/Server.cs
@@ -14,6 +14,7 @@ var buffer = new byte[1024];
 sock.Receive(buffer);
 
 var bufferStringify = Encoding.UTF8.GetString(buffer);
+Console.WriteLine(bufferStringify);
 var path = bufferStringify.Split(" ")[1].Split("/")[0];
 
 if (path.Length > 0) {
