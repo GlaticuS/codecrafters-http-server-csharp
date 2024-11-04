aa7c53cc32ab38f628ff70dd70a1d3c6352645e2
GlaticuS <glaticus@mail.ru>
2024-11-05 00:16:12 +0300

    completed

--- src/Server.cs
+++ src/Server.cs
@@ -15,7 +15,7 @@ namespace codecrafters_http_server.src
             router.Register(typeof(EchoController));
 
             // You can use print statements as follows for debugging, they'll be visible when running tests.
-            Console.WriteLine("Logs from your program will appear here!");
+            //Console.WriteLine("Logs from your program will appear here!");
 
             // Uncomment this block to pass the first stage
             TcpListener server = new TcpListener(IPAddress.Any, 4221);
