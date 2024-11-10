632b9b1e9caaba2ac3c889b5ee716ea0cd5004ae
GlaticuS <glaticus@mail.ru>
2024-11-10 21:30:58 +0300

    ch

--- src/HttpRequestContext.cs
+++ src/HttpRequestContext.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace codecrafters_http_server.src
 {
-    public class HttpRequestContext(string path, string method, Dictionary<string, string> headers)
+    public class HttpRequestContext(string path, string method, Dictionary<string, string> headers, string body)
     {
         public string Path { get; private set; } = path;
         public string Method { get; private set; } = method;
         public Dictionary<string, string> Headers { get; private set; } = headers;
+        public string Body { get; private set; } = body;
     }
 }
